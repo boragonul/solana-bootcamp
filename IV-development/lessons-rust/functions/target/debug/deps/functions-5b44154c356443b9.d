921ace1a4444b0ed0f303ef305d67e412c049544
Bora Gönül <me@boragonul.com>
2022-11-18 15:08:45 +0300

    del: target of functions

--- IV-development/lessons-rust/functions/target/debug/deps/functions-5b44154c356443b9.d
+++
@@ -1,5 +0,0 @@
-/home/borag/Code/work/solana-bootcamp/IV-development/lessons/functions/target/debug/deps/functions-5b44154c356443b9.rmeta: src/main.rs
-
-/home/borag/Code/work/solana-bootcamp/IV-development/lessons/functions/target/debug/deps/functions-5b44154c356443b9.d: src/main.rs
-
-src/main.rs:
